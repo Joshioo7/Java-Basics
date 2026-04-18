9a546ba35ab76aeb237bbe63da6891fda102b315
Tejasva joshi <116024359+Joshioo7@users.noreply.github.com>
2026-04-18 15:21:43 +0530

    Add files via upload

---
+++ lastoccurrence.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class lastoccurrence{
+
+public static int lastOccurrence(int arr[], int target) {
+    int start = 0;
+    int end = arr.length - 1;
+    int ans = -1;
+
+    while(start <= end) {
+        int mid = (start + end) / 2;
+
+        if(arr[mid] == target) {
+            ans = mid;        
+            start = mid + 1; 
+        } 
+        else if(arr[mid] < target) {
+            start = mid + 1;
+        } 
+        else {
+            end = mid - 1;
+        }
+    }
+
+    return ans;
+}
+
+    public static void main(String[]  args){
+        Scanner TJ = new Scanner(System.in);
+        System.out.println("Enter total number of elements");
+        int n = TJ.nextInt();
+        int arr[] = new int[n];
+
+        System.out.println("Enter elements");
+        for(int i=0; i<n ;i++){
+            arr[i]=TJ.nextInt();
+        }
+
+        System.out.println("Enter target");
+        int target = TJ.nextInt();
+        int result = lastOccurrence(arr , target);
+
+        if(result!=-1){
+            System.out.println("Element found last at " + result);
+        } else { 
+            System.out.println("Element not found");
+        }
+     }   
+}
