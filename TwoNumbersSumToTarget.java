be4829208137de0f38ee17b8e69a22cf3cd8f1a0
Tejasva joshi <116024359+Joshioo7@users.noreply.github.com>
2026-04-25 00:48:33 +0530

    Add files via upload

---
+++ TwoNumbersSumToTarget.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class TwoNumbersSumToTarget { 
+
+    public static boolean Istargetsum(int arr[], int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int sum = arr[start] + arr[end];
+
+            if (sum == target) {
+                return true;
+            } else if (sum < target) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner TJ = new Scanner(System.in);
+
+        System.out.println("Enter total elements");
+        int n = TJ.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter elements");
+        for (int i = 0; i < n; i++) {
+            arr[i] = TJ.nextInt();
+        }
+
+        System.out.println("Enter target");
+        int target = TJ.nextInt();
+
+        // ✅ Correct function call
+        boolean result = Istargetsum(arr, target);
+
+        // ✅ Correct variable used
+        if (result) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
