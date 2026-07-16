395bb740f2fd044c385d70ea4b269ae22666ff27
Tejasva joshi <116024359+Joshioo7@users.noreply.github.com>
2026-07-16 22:58:05 +0530

    Add files via upload

---
+++ methodsnew.java
@@ -0,0 +1,11 @@
+public class methodsnew{
+    public static void main(String[] args){
+        methodsnew D = new methodsnew();
+        D.getdata();
+    }
+
+    public void getdata(){
+        System.out.println("Hello World");
+
+    }
+}
