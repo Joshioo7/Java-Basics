8923ddbdee59ebb33897d2f16d4752d76f405caa
Tejasva joshi <116024359+Joshioo7@users.noreply.github.com>
2026-07-16 23:12:56 +0530

    Add files via upload

--- methodsnew.java
+++ methodsnew.java
@@ -2,10 +2,15 @@ public class methodsnew{
     public static void main(String[] args){
         methodsnew D = new methodsnew();
         D.getdata();
+        String name = D.getdata(); //now here we are saving vale returned from getdata() in name variable which is storing string value
+        System.out.println(name);
     }
 
-    public void getdata(){
+    public String getdata(){
         System.out.println("Hello World");
+        return ("Tejasva Joshi");
+
+        // in this ocde , we made an object , then called the method using syntax object.method name
 
     }
 }
